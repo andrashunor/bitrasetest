bef1193c26220e84c0fd666ab3a916af983db25c
Hunor Andras <handras@halcyonmobile.com>
2018-03-26 09:12:30 +0300

    Initial commit.

---
+++ bitrasetest/bitrasetest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitrasetest
+//
+//  Created by Hunor Andras on 26/03/2018.
+//  Copyright © 2018 Hunor Andras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
